d83c4faba3e8ffccf054188e044f11d871ba0055
elieiradukunda <iradukundaelie71@gmail.com>
2025-07-30 09:39:47 +0200

    landing page yahoo updated

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors:{
+        greenapple: '#36A420',
+      },
+    },
+  },
+  plugins: [],
+}
+
